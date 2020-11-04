4503db439c5f609c321368e4aa91dcf3e9fd0212
810807976 <810807976@qq.com>
2020-11-04 10:08:58 +0800

    上传作业

---
+++ db_homework/src/main/java/org/scu_db/demo/service/BookService.java
@@ -0,0 +1,17 @@
+package org.scu_db.demo.service;
+
+import org.scu_db.demo.model.Book;
+
+import java.util.List;
+
+public interface BookService {
+    List<Book> findAllBooks();
+    Book findBookById(Integer Id);
+    List<Book> findBookByIdUseSql(Integer Id);
+    Integer deleteBookById(Integer id);
+    Integer updateBook(Book book, Integer method);
+    List<Book> findBookedBook();
+    List<Book> findBookByCallnumber(String callnumber);
+
+    List<String> findCallnumbersLessThanNum(Integer num);
+}
